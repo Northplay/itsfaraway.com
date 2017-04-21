af01b106c0b332949aa6adb67e8d47467994ae1f
Kristian Andersen <hello@kristian.co>
2017-04-21 11:39:16 +0200

    Added signup

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,49 @@ var $ = require('jquery');
 var Ladda = require('ladda');
 var Retina = require('retina.js').Retina;
 
+const handle_newsletter_submit = function(e) {
+  e.preventDefault();
+  var $email = $(this).find('input[name=email]');
+  var $submit = $(this).find('input[type=submit]');
+  var l = Ladda.create(document.querySelector('.ladda-button'));
+  l.start();
+
+  if ($email.val() == '') {
+    Ladda.stopAll();
+    return;
+  }
+
+  $email.prop('disabled', true);
+  $submit.prop('disabled', true);
+
+  const data = {
+    email: $email.val(),
+    list: 'faraway'
+  };
+  const url = 'https://services.northplay.co/subscribe';
+
+  $.ajax({
+    url: url,
+    method: 'POST',
+    cache: false,
+    contentType: 'application/json',
+    data: JSON.stringify(data),
+    success: function (data) {
+      $('.newsletter__form').hide();
+      $('.newsletter__thanks').show();
+      Ladda.stopAll();
+    },
+    error: function (xhr, status, error) {
+      $email.prop('disabled', false);
+      $submit.prop('disabled', false);
+      Ladda.stopAll();
+    }
+  });
+};
+
 $(document).ready(function() {
   Ladda.stopAll();
   Retina.init(window);
+
+  $('#newsletter_form').on('submit', handle_newsletter_submit);
 });
